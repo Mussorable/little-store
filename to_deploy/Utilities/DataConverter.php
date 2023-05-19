81a0b862e766013788b264b13bd811fe154b1c89
Oleh Diakov <redduck5601@gmail.com>
2023-05-19 10:58:16 +0200

    php files refactoring

--- to_deploy/Utilities/DataConverter.php
+++ to_deploy/Utilities/DataConverter.php
@@ -1,29 +1,35 @@
 <?php
 
+
 namespace Utilities;
+
+use Utilities\ProductTypes;
 
 class DataConverter
 {
-    private $productType;
-    private $sku;
-    private $name;
-    private $price;
-
-    function __construct($productType, $sku, $name, $price)
+    public static function getProductClass($productType, $sku, $name, $price)
     {
-        $this->productType = $productType;
-        $this->sku = $sku;
-        $this->name = $name;
-        $this->price = $price;
+        if (class_exists(ProductTypes::class . '\\' . $productType)) {
+            $className = ProductTypes::class . '\\' . $productType;
+            $class = new $className($sku, $name, $price);
+
+            $obj = new self();
+            $obj->getTypeClassData($class);
+
+            return $class;
+        }
     }
 
-    final public function factory()
+    private function getTypeClassData($product)
     {
-        // 
-        require_once('product_list/Products/' . $productType . '.php');
-        if (class_exists($productType)) {
-            $class = new $productType($sku, $name, $price);
-            return $class;
+        if ($product instanceof ProductTypes\Book) {
+            $product->setData($_GET['weight']);
+        } elseif ($product instanceof ProductTypes\DVD) {
+            $product->setData($_GET['size']);
+        } elseif ($product instanceof ProductTypes\Furniture) {
+            $product->setData($_GET['height'], $_GET['width'], $_GET['length']);
+        } else {
+            return "Error. Product type nod defined.";
         }
     }
 }
--- to_deploy/deleteItems.php
+++ to_deploy/deleteItems.php
@@ -1,19 +1,10 @@
 <?php
 
-require 'Utilities/autoloader.php';
+require_once 'Utilities/Autoloader.php';
 
 use Utilities\ConnectDB;
 
 
 $database = new ConnectDB('localhost', 'products', 'root', 'root');
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $dataArray = json_decode(file_get_contents('php://input'), true);
-
-    if (isset($dataArray['data']) && is_array($dataArray['data'])) {
-        $receivedArray = $dataArray['data'];
-        foreach ($receivedArray as $index => $value) {
-            echo $database->delProducts($value);
-        }
-    }
-}
+$database->delProducts();
+$database->closeConnection();
